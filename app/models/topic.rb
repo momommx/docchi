19be1ef88def942cf928d2e52733f2160ad95af9
momommx <106795266+momommx@users.noreply.github.com>
2022-08-06 07:31:55 +0000

    [Add] routes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,9 @@ class Topic < ApplicationRecord
   has_many :answers,   dependent: :destroy
   belongs_to :genre,   deoendent: :destroy
   belongs_to :user,    dependent: :destroy
+  
+  def favorited_by?(user)
+  　favorites.exists?(user_id: user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,35 @@
 Rails.application.routes.draw do
+  
   # 管理者用
   # [URL] /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] , controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
   }
   
   get "admin" => "admin/answers#index"
   namespace :admin do
     resources :answers, only: [:index, :destroy]
+    resources :genres,  only: [:index, :create, :destroy]
+    resources :users,   only: [:index, :update]
   end
   
 
-
-
-    
   # ユーザ用
   # [URL] /users/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
- 
-  root to: 'public/homes#top'
   
+  root to: 'homes#top'  
+  
+  scope :public do   # scopeメソッドを用いて、Prefixのpublicのみを消す。
+    resources :topics, only: [:index, :show, :new] do
+      resource  :favorites, only: [:create, :destroy]
+      resources :answers,   only: [:create, :destroy]
+    end
+    resources :users, only: [:show]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
