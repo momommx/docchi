eace45cbfac117070bedffa6660e9ad5fe70fa5f
momommx <106795266+momommx@users.noreply.github.com>
2022-08-06 14:16:18 +0000

    [Add] Header & Footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  #Sign_upの際に、ユーザー名(name)のデータ操作を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
     
   #非ログイン時でも全ページ閲覧可能にする
   #before_action :login_required
  
-  private
+  #private
   
   #全てのアクションの処理前にユーザーがログイン済みかどうか確認する
   #def login_required 
