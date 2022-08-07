9ef442a7822db6ac4d6a44a1b828fdd58889be50
momommx <106795266+momommx@users.noreply.github.com>
2022-08-07 07:00:40 +0000

    [Update] Log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource) 
+    public_topics_path
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    elsif resource == :user
+      root_path
+    end
+  end
+
 
   protected
 
@@ -7,6 +20,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+  
     
   #非ログイン時でも全ページ閲覧可能にする
   #before_action :login_required
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,10 +2,6 @@
 
 class Public::SessionsController < Devise::SessionsController
   #before_action :configure_sign_in_params, only: [:create]
-  
-  def after_sign_in_path_for(resource)
-    root_path
-  end
 
   # GET /resource/sign_in
   # def new
@@ -23,11 +19,6 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   #protected
-
-
-
-
-
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
