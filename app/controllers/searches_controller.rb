fa2259fed94962dc5446d1c5e3e37dc70ed00b71
momommx <106795266+momommx@users.noreply.github.com>
2022-08-14 08:17:30 +0000

    [Update] Title on Search result page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,9 +5,11 @@ class SearchesController < ApplicationController
     @topics = Topic.all
     # お題検索で絞り込まれたwordが@topicsに代入される。  
     @topics = Topic.looks(params[:word])
-    # ジャンル検索でジャンルが指定された場合、@topicsの中からジャンルを絞り込む。
+    # ジャンル検索でジャンルが指定された場合、@topicsの中からさらにジャンルを絞り込む。
     @topics = @topics.where(genre_id: params[:genre_id]) if params[:genre_id].present?
     @count = @topics.count
+    #ジャンル検索でgenre_idが指定された場合、そのgenre_idのレコードを＠genreに代入。
+    @genre = Genre.find(params[:genre_id]) if params[:genre_id].present?
   end
 
 end
