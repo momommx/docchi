1a125870483b8fe19f2f6ba28de8782a660e0413
momommx <106795266+momommx@users.noreply.github.com>
2022-08-23 13:16:15 +0000

    [Fix] assets/stylesheets/application.cssの記述をjavascript/stylesheets/application.scssへ移動, [Fix] Pagination

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,6 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    @topics = Topic.all
     # お題検索で絞り込まれたwordが@topicsに代入される。  
     @topics = Topic.looks(params[:word])
     # ジャンル検索でジャンルが指定された場合、@topicsの中からさらにジャンルを絞り込む。
@@ -10,6 +9,8 @@ class SearchesController < ApplicationController
     @count = @topics.count
     #ジャンル検索でgenre_idが指定された場合、そのgenre_idのレコードを＠genreに代入。
     @genre = Genre.find(params[:genre_id]) if params[:genre_id].present?
+    
+    @topics = @topics.page(params[:page])
   end
 
 end
