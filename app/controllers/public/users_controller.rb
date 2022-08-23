ed66a702f8158ed0abfa3c826fc721a9fbec0042
momommx <106795266+momommx@users.noreply.github.com>
2022-08-23 06:17:19 +0000

    [Fix] Layout

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -5,7 +5,7 @@ class Public::UsersController < ApplicationController
   def show
     @users = User.all
     @user = User.find(params[:id])
-    @topics = @user.topics
+    @topics = @user.topics.order(created_at: :desc)
   end 
  
   def favorites
@@ -14,7 +14,7 @@ class Public::UsersController < ApplicationController
     # いいねした投稿を探し、@favorite_postsに格納
     favorites = Favorite.where(user_id: @user.id).pluck(:topic_id)
     @favorite_topics = Topic.find(favorites)
-  end 
+  end
  
  
   private
