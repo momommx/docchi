58558c3c27e55f4368e68e8b35e9385af245e91a
momommx <106795266+momommx@users.noreply.github.com>
2022-08-24 13:04:43 +0000

    [Fix] Bootstrapの修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,12 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
 import "popper.js";
-import "bootstrap";
-import "../stylesheets/application.scss";
+import 'bootstrap';
+import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/js/all';
+
+
+
 
 Rails.start()
 Turbolinks.start()
