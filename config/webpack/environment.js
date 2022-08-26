0ebc6c5606e0ebab2bb39d9d08f189795a92dd38
momommx <106795266+momommx@users.noreply.github.com>
2022-08-26 14:51:26 +0000

    [Fix] 本番環境でBootstrapが反映されない不具合, [Fix] Layout

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
-
 const webpack = require('webpack')
+
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
@@ -11,4 +10,6 @@ environment.plugins.prepend(
     Popper: 'popper.js'
   })
 )
+
+module.exports = environment
 
